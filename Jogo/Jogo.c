b734be3c13df3b3a0e14b3a39182fa86d865f91e
Vitor de Souza <vitordesouza681@gmail.com>
2022-09-29 20:05:34 -0300

    Jogo v3
    Jogo funcionando

--- Jogo/Jogo.c
+++ Jogo/Jogo.c
@@ -8,30 +8,29 @@ int main (void){
     char c;
 
     srand(time(NULL));
-    sorteio = rand() % 1000;
+    sorteio = rand() % 100;
+    printf ("\n\nBem vindo ao jogo!\nTente acertar o numero com o menos de tentativa que conseguir!\n\n");
 
     do{
         tentativa++;
         printf("\ntentativa numero %d", tentativa);
+        printf("\ndigite 0 para sair");
         printf("\ntente advinhar o numero: ");
         scanf("%d", &num);
-
+        if (num!=0){
         if(num==sorteio){
-            printf("acertou!");
+            printf("\n\nacertou miseravi!");
             break;
         }
-                if(num<sorteio){
-                    printf("numero menor do que o sortiado");
-                }
-                else{
-                    printf("numero maior do que o sortiado");
-                }
-            printf ("\ntente de novo? (S/N)");
-            fflush(stdin);
-            scanf ("%c", &c);
-        }while(c=='s' || c=='S');
+            if(num<sorteio){
+                printf("\nnumero menor do que o sortiado\n");
+            }
+            else{
+                printf("\nnumero maior do que o sortiado\n");
+            }
         
-
-        return 0;
-    }
+        }
+    }while(num!=0 || num==sorteio);       
+    return 0;
+}
    
