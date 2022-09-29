4b2be5a6ee5d7a23ce18adf3e95178c0870981b2
Vitor de Souza <vitordesouza681@gmail.com>
2022-09-29 20:11:16 -0300

    Jogo version final

--- Jogo/Jogo.c
+++ Jogo/Jogo.c
@@ -2,35 +2,39 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main (void){
+int main(void)
+{
 
-    int num, sorteio, tentativa=0;
+    int num, sorteio, tentativa = 0;
     char c;
 
     srand(time(NULL));
     sorteio = rand() % 100;
-    printf ("\n\nBem vindo ao jogo!\nTente acertar o numero com o menos de tentativa que conseguir!\n\n");
+    printf("\n\nBem vindo ao jogo!\nTente acertar o numero com o menos de tentativa que conseguir!\n\n");
 
-    do{
+    do
+    {
         tentativa++;
         printf("\ntentativa numero %d", tentativa);
         printf("\ndigite 0 para sair");
         printf("\ntente advinhar o numero: ");
         scanf("%d", &num);
-        if (num!=0){
-        if(num==sorteio){
-            printf("\n\nacertou miseravi!");
-            break;
-        }
-            if(num<sorteio){
+        if (num != 0)
+        {
+            if (num == sorteio)
+            {
+                printf("\n\nacertou miseravi!");
+                break;
+            }
+            if (num < sorteio)
+            {
                 printf("\nnumero menor do que o sortiado\n");
             }
-            else{
+            else
+            {
                 printf("\nnumero maior do que o sortiado\n");
             }
-        
         }
-    }while(num!=0 || num==sorteio);       
+    } while (num != 0 || num == sorteio);
     return 0;
 }
-   
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Jogo
- jogo de advinhação em C
+ jogo de adivinhação em C
+ Jogo de adivinhação escirto em C, com o objetivo de identificar o numero sorteado que varia de 1 a 100, o numero é gerado aleatoriamente usando o a opção (time(NULL)) que seria referente ao contador de horas do windowns. O jogo além disso conta com um contador de contativas, um escape para sair do jogo, e resposta de acerto.
